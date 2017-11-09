cd2e482ad9f7d93dbd45ee286041d37a7115d839
Wade Tobin <wadetobin@mwc-102182.dhcp.missouri.edu>
2017-11-09 16:12:25 -0600

    Stage 1 Conversion Calculator

--- ConversionCalculator/ConverterViewController.swift
+++ ConversionCalculator/ConverterViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ConverterViewController: UIViewController {
 
+    @IBOutlet weak var outputDisplay: UITextField!
+    @IBOutlet weak var inputDisplay: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        outputDisplay.text = Converter.init().outputUnit[0]
+        inputDisplay.text = Converter.init().inputUnit[0]
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +27,53 @@ class ConverterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func converterTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Choose Converter", message: "", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        alert.addAction(UIAlertAction(title: Converter.init().labels[0], style: UIAlertActionStyle.default, handler: { (alertAction) -> Void in
+            
+            self.outputDisplay.text = Converter.init().outputUnit[0]
+            self.inputDisplay.text = Converter.init().inputUnit[0]
+            
+        }))
+        alert.addAction(UIAlertAction(title: Converter.init().labels[1], style: UIAlertActionStyle.default, handler: { (alertAction) -> Void in
+            
+            self.outputDisplay.text = Converter.init().outputUnit[1]
+            self.inputDisplay.text = Converter.init().inputUnit[1]
+            
+        }))
+        alert.addAction(UIAlertAction(title: Converter.init().labels[2], style: UIAlertActionStyle.default, handler: { (alertAction) -> Void in
+            
+            self.outputDisplay.text = Converter.init().outputUnit[2]
+            self.inputDisplay.text = Converter.init().inputUnit[2]
+        }))
+        alert.addAction(UIAlertAction(title: Converter.init().labels[3], style: UIAlertActionStyle.default, handler: { (alertAction) -> Void in
+            
+            self.outputDisplay.text = Converter.init().outputUnit[3]
+            self.inputDisplay.text = Converter.init().inputUnit[3]
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    struct Converter {
+        let labels = ["Fahrenheit to Celcius", "Celcius to Fahrenheit", "Miles to Kilometers", "Kilometers to Miles"]
+        let inputUnit = ["°F", "°C", "mi", "km"]
+        let outputUnit = ["°C", "°F", "km", "mi"]
+        
+    }
+    
+    
+    
+//    func farenheitToCelcius(farenheit: Double) -> String {
+//        let celcius = ((farenheit + fraction) - 32.0) * (5.0/9.0)
+//        return String(celcius)
+//    }
+//
+//    func kilometeresToMiles(speedInKPH: Double) -> String{
+//        let speedInMPH = (speedInKPH + fraction) / 1.60934
+//        return String(speedInMPH)
+//    }
 
     /*
     // MARK: - Navigation
